2160774e34cce90c54a1d752a203ab4a581dbb19
Samson Zhang <wwsamson@yahoo.com>
2021-06-22 09:42:37 -0700

    add more essays on writing page

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -21,8 +21,28 @@ export default function Writing({}: {  }) {
             url: "https://www.theincandescentreview.org/post/what-does-it-mean-to-be-asian-american-a-narrative-of-coronavirus-andrew-yang-and-america",
         },
         {
+            title: "How to Innovate and Create a Culture of Innovation",
+            url: "https://samsonzhang.medium.com/how-to-innovate-and-create-a-culture-of-innovation-fb6614f8bc1",
+        },
+        {
             title: "How to Have Original Thoughts: The Importance of Tradition and Asking Bad Questions",
             url: "https://postulate.us/@samsonzhang/p/2021-03-12-How-to-Have-Original-Thoughts%3A-3fXWPXwup49YMJF6ZLPHQy",
+        },
+        {
+            title: "The Sync Gap: How to Be Creative in Conversation, Writing, and Work",
+            url: "https://samsonzhang.medium.com/the-sync-gap-how-to-be-creative-in-conversation-writing-and-work-dd5ef61f608c",
+        },
+        {
+            title: "Why Hustle? Hustle Culture as a Survival Mechanism and Driver of Human Progress",
+            url: "https://samsonzhang.medium.com/why-hustle-hustle-culture-as-a-mechanism-to-seek-belonging-and-change-the-world-6980def00678",
+        },
+        {
+            title: "Media Shapes Reality: The Purpose of Journalism | Notes on Ch. 1 of \"The Elements of Journalism\"",
+            url: "https://postulate.us/@samsonzhang/p/2021-05-24-Media-Shapes-Reality%3A-The-Purpose-7kZ8unB7c2NnXYkJUVzMLV",
+        },
+        {
+            title: "Revolutionary Social-Democracy as the Vanguard of All Democratic Struggle | Notes on Chapter 3 of Lenin's \"What Is To Be Done?\"",
+            url: "https://postulate.us/@samsonzhang/p/2021-04-04-Revolutionary-Social-Democracy-as-the-Vanguard-2bgBtNg5Fso7e2rxJH4PCR",
         },
     ];
 
