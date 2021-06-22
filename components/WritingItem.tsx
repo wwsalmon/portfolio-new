2f341a287c5a67a6479e327206b01cb50ac26b16
Samson Zhang <wwsamson@yahoo.com>
2021-06-21 22:16:30 -0700

    fix spacing on writing page

--- components/WritingItem.tsx
+++ components/WritingItem.tsx
@@ -3,7 +3,7 @@ import {WritingItemObj} from "../utils/types";
 export default function WritingItem({item}: { item: WritingItemObj }) {
     return (
         <div className="my-8">
-            <p className="font-serif text-gray2">{item.publication ? <i>{item.publication}</i> : "Personal"}</p>
+            <p className="font-serif text-gray2 mb-1">{item.publication ? <i>{item.publication}</i> : "Personal"}</p>
             <a href={item.url} className="font-serif text-xl" style={{lineHeight: 1.1}}>{item.title}</a>
         </div>
     );
